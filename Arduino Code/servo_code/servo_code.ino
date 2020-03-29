e6b1423443e50a6b9a1e827dadf5b77606637611
EToninelli <42411573+EToninelli@users.noreply.github.com>
2020-03-29 20:32:36 +0100

    milestone

--- Arduino Code/servo_code/servo_code.ino
+++ Arduino Code/servo_code/servo_code.ino
@@ -2,7 +2,7 @@
 
 Servo servoL;
 Servo servoR;
-int wait = 1500; // pause between pumps
+int wait = 2000; // pause between pumps
 
 int small_angle = 500; // servo lower limit
 int big_angle = 2500; // servo higher limit
@@ -18,15 +18,16 @@ delay(2000);
 } 
 
 void loop() { 
-servoL.writeMicroseconds(big_angle-150);  // set servoL to lower_limit
+servoL.writeMicroseconds(big_angle-125);  // set servoL to lower_limit
 
 
 servoR.writeMicroseconds(small_angle);  // set servoL to lower_limit
 delay(wait);
 
-servoL.writeMicroseconds(1650); // set servoL to higher_limit
+//servoL.writeMicroseconds(1650); // set servoL to higher_limit
+servoL.writeMicroseconds(small_angle+525); // set servoL to higher_limit
 
-
-servoR.writeMicroseconds(big_angle/2); // set servoL to higher_limit
+//servoR.writeMicroseconds(big_angle/2); // set servoL to higher_limit
+servoR.writeMicroseconds(big_angle-650); // set servoL to higher_limit
 delay(wait);
 }
